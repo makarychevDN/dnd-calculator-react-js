85e8ebf82150d6b7bbb6211dbe14597d171427cd
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2024-12-04 16:43:31 +0500

    tha layout of throw d20 panel has been restored in react js

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import './GenericGroupLayout.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='generic-gruop-layout'>
+        <div style={{display: "flex", minWidth: "max-content"}}>
+          <div>
+          <label style={{fontSize: "larger"}}><b>Попадание</b></label>
+          <p></p>
+            <div>
+              <label style={{color: "gray"}}>модификатор </label>            
+              <select id="selected-modificator">
+                <option value="strength">силы 5</option>
+                <option value="dexterity">ловкости 5</option>
+                <option value="constitution">выносливости 5</option>
+                <option value="intelligence">интеллекта 5</option>
+                <option value="wisdom">мудрости 5</option>
+                <option value="charisma">харизмы 5</option>
+              </select>
+            </div>
+            <div><label style={{color: "gray"}}>бонус мастерства 3</label></div>
+            <div><label style={{color: "gray"}}>дайс</label></div>
+          </div>
+          <div style={{width: "fit-content", textAlign: "center", marginRight: "40px", marginLeft: "auto", marginTop: "auto", marginBottom: "auto"}}>
+            <div><label style={{fontSize: "xx-large"}}>13</label></div>
+            <div><label style={{color: "gray"}}></label></div>
+          </div>
+        </div>
+        <hr/>
+        <button>d20</button>
+        <button>d20 с преимуществом</button>
+        <button>d20 с помехой</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }

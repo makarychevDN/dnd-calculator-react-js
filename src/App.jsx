21bb33d78580e77cddfcf45a5d1029d223e099bc
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2024-12-09 16:17:54 +0500

    the mockup of damage panel has been added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import ThrowD20Panel from './scripts/components/throwD20Panel'
+import CalculateDamagePanel from './scripts/components/CalculateDamagePanel'
 import './styles/GenericGroupLayout.css'
 import './styles/DiceButton.css'
 
@@ -10,6 +11,7 @@ function App() {
       <ThrowD20Panel 
         character={currentCharacter}
       />
+      <CalculateDamagePanel />
     </>
   )
 }

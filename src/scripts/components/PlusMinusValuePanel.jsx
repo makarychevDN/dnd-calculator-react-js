875e2cac3f29a402496320fd2053a7a4666ce16c
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2024-12-20 00:21:43 +0500

    now it has add and substract functions

--- src/scripts/components/PlusMinusValuePanel.jsx
+++ src/scripts/components/PlusMinusValuePanel.jsx
@@ -1,13 +1,52 @@
+import { useState } from "react";
+
 function PlusMinusValuePanel(props) {
+
+  const [inputFieldValue, setInputFieldValue] = useState(1);
+
   return(
     <>
-        <div id="group-1" style={{width: "fit-content", textAlign: "center", marginRight: "0px", marginLeft: "10px", marginTop: "auto", marginBottom: "auto"}}>
-          <button className="generic-interactive-button">-</button>   
-          <input type="text" style={{textAlign: "center"}} className="generic-text-input" />
-          <button className="generic-interactive-button">+</button>
+        <div style={{width: "fit-content", textAlign: "center", marginRight: "0px", marginLeft: "10px", marginTop: "auto", marginBottom: "auto"}}>
+          <button className="generic-interactive-button" onClick={substract}>-</button>   
+          <input type="text" style={{textAlign: "center"}} className="generic-text-input" value={inputFieldValue} onInput={removeUnexpectedSymbols} />
+          <button className="generic-interactive-button" onClick={add}>+</button>
         </div>
     </>
   );
+
+  function removeUnexpectedSymbols(e) {
+    let correctedValue = e.target.value;
+
+    correctedValue = Array.from(correctedValue)
+      .filter(char => !isNaN(Number(char)))
+      .join("");
+
+    while(correctedValue[0] == "0"){
+        correctedValue = correctedValue.slice(1);
+    }
+
+    if(!correctedValue){
+        correctedValue = 0;
+    }
+
+    setInputFieldValue(correctedValue);
+  }
+
+  function add(){
+    if(props.onAdd){
+      props.onAdd(inputFieldValue);
+    }
+
+    setInputFieldValue(1);
+  }
+
+  function substract(){    
+    if(props.onSubstract){
+      props.onSubstract(inputFieldValue);
+    }
+
+    setInputFieldValue(1);
+  }
 }
 
 export default PlusMinusValuePanel

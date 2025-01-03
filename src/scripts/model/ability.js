ac723e3b1e257e2c9fc1a073a46fe8832a7095b2
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2025-01-03 22:37:21 +0600

    ability scripts have been added

---
+++ src/scripts/model/ability.js
@@ -0,0 +1,7 @@
+class Ability{
+  constructor(name, abilityOptions, currentAbilityOption){
+    this._name = name;
+    this._abilityOptions = abilityOptions;
+    this._currentAbilityOption = currentAbilityOption;
+  }
+}
---
+++ src/scripts/model/abilityOption.js
@@ -0,0 +1,8 @@
+class AbilityOption{
+  constructor(name, damageType, dicesQuantity, dicesMaxValue, additionalValue){ 
+    this._name = name;
+    this._dicesQuantity = dicesQuantity;
+    this._dicesMaxValue = dicesMaxValue;
+    this._additionalValue = additionalValue;
+  }
+}

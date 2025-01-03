55f8037bbc4e8fac9b1c79cfbd4582cb051e16f8
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2025-01-04 00:47:22 +0600

    the missing field has been added

--- src/scripts/model/abilityOption.js
+++ src/scripts/model/abilityOption.js
@@ -1,6 +1,7 @@
 class AbilityOption{
   constructor(name, damageType, dicesQuantity, dicesMaxValue, additionalValue){ 
     this._name = name;
+    this._damageType = damageType;
     this._dicesQuantity = dicesQuantity;
     this._dicesMaxValue = dicesMaxValue;
     this._additionalValue = additionalValue;
